37240ab30f9d7bc62c8061c2b472c096ba145f4e
Paulo Sampaio <paulohenrique.sales47@gmail.com>
2020-01-30 22:55:24 -0300

    Utilizando GridView para montagem da interface e MediaQuery para recuperar informações sobre o dispositivo (largura, altura da tela e aspect ratio)

--- lib/Animais.dart
+++ lib/Animais.dart
@@ -6,8 +6,39 @@ class Animais extends StatefulWidget {
 }
 
 class _AnimaisState extends State<Animais> {
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+
+    double _largura = MediaQuery.of(context).size.width;
+    double _altura = MediaQuery.of(context).size.height;
+    print(_altura);
+    print(_largura);
+    print(MediaQuery.of(context).size.aspectRatio);
+
+    return GridView.count(
+      crossAxisCount: 2,
+      childAspectRatio: MediaQuery.of(context).size.aspectRatio * 2,
+      children: <Widget>[
+        GestureDetector(
+          child: Image.asset("assets/images/cao.png"),
+        ),
+        GestureDetector(
+          child: Image.asset("assets/images/gato.png"),
+        ),
+        GestureDetector(
+          child: Image.asset("assets/images/leao.png"),
+        ),
+        GestureDetector(
+          child: Image.asset("assets/images/macaco.png"),
+        ),
+        GestureDetector(
+          child: Image.asset("assets/images/ovelha.png"),
+        ),
+        GestureDetector(
+          child: Image.asset("assets/images/gato.png"),
+        ),
+      ],
+    );
   }
 }
